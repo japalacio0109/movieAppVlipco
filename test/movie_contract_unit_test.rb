40bac1c6917f7f69af1e697f5bd49fa67cf7cb31
Jeyson Palacio <japalacio0108@gmail.com>
2019-10-07 06:27:50 -0500

    Update test

--- test/movie_contract_unit_test.rb
+++ test/movie_contract_unit_test.rb
@@ -5,19 +5,6 @@ require_relative '../api/models/movie/movie_contract'
 
 class MovieContractTest < TestBase
 
-  def test_movie_contract_empty
-    contract = MovieContract.new
-    reservation = get_reservation_fixtures()[:error_by_empty]
-    reservation = reservation[:movie]
-    result = contract.call(reservation)
-    assert !result.success?
-    assert result.errors.to_h[:name].include?("Field is required")
-    assert result.errors.to_h[:description].include?("Field is required")
-    assert result.errors.to_h[:image_url].include?("Field is required")
-    assert result.errors.to_h[:since_at].include?("Field is required")
-    assert result.errors.to_h[:until_at].include?("Field is required")
-  end
-
   def test_movie_contract_success
     contract = MovieContract.new
     reservation = get_movie_fixtures()[:true_assert_on_check]
@@ -26,14 +13,6 @@ class MovieContractTest < TestBase
     assert result.success?
   end
 
-  def test_movie_contract_fail_by_name
-    contract = MovieContract.new
-    reservation = get_movie_fixtures()[:error_by_name]
-    reservation = reservation[:movie]
-    result = contract.call(reservation)
-    assert !result.success?
-    assert result.errors.to_h[:name].include?("Field is required")
-  end
 
   def test_movie_contract_fail_by_short_description
     contract = MovieContract.new
--- test/reservation_contract_unit_test.rb
+++ test/reservation_contract_unit_test.rb
@@ -4,19 +4,6 @@ require_relative './test_base'
 require_relative '../api/models/reservation/reservation_contract'
 
 class ReservationContractTest < TestBase
-
-  def test_reservation_contract_empty
-    contract = ReservationContract.new
-    reservation = get_reservation_fixtures()[:error_by_empty]
-    reservation = reservation[:reservation]
-    result = contract.call(reservation)
-    assert !result.success?
-    assert result.errors.to_h[:movie_id].include?("Field is required")
-    assert result.errors.to_h[:position].include?("Field is required")
-    assert result.errors.to_h[:position].include?("Value must be between (1,10) range")
-    assert result.errors.to_h[:reservation_date].include?("Field is required")
-    assert result.errors.to_h[:created_at].include?("Field is required")
-  end
   
   def test_reservation_contract_success
     contract = ReservationContract.new
@@ -25,16 +12,7 @@ class ReservationContractTest < TestBase
     result = contract.call(reservation)
     assert result.success?
   end
-
-  def test_reservation_contract_fail_by_movie_id
-    contract = ReservationContract.new
-    reservation = get_reservation_fixtures()[:error_by_movie_id]
-    reservation = reservation[:reservation].merge(created_at: Time.now)
-    result = contract.call(reservation)
-    assert !result.success?
-    assert result.errors.to_h[:movie_id].include?("Field is required")
-  end
-
+  
   def test_reservation_contract_fail_by_out_range_position
     contract = ReservationContract.new
     reservation = get_reservation_fixtures()[:error_by_out_range_position]
